f0ece64eb0dccd324cba75bb0dc3c24ffc43b0fd
romimelfidata <rominamelfi@gmail.com>
2023-04-02 16:11:30 -0300

    sin comillas

--- ejemplo_cicloDefinido_for.py
+++ ejemplo_cicloDefinido_for.py
@@ -1,14 +1,14 @@
-"""Se requiere ingresar las notas de 10 alumnos y calcular su promedio.
-"""
+#Actividad:
+# Se requiere ingresar las notas de 10 alumnos y calcular su promedio.
 
 #Inicio de contadores
 suma = 0
 cantidad = 10
 
-# Se ingresan las notas con range (Secuencia de número), TRUE, se sigue leyendo
 for x in range(cantidad):
-    nota = input("Ingrese la nota del alumno ")
+    # Se ingresan las notas con range (Secuencia de número), TRUE, se sigue leyendo
+    nota = input("Ingrese la nota del alumno: ")
     suma += int(nota)
-    
 promedio = suma/cantidad
+
 print("EL promedio de calificaciones es", promedio)
